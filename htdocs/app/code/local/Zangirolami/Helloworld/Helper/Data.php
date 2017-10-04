3011dcbeb6757115864b88d12d75604850a872c3
Michael <mikezangirolami@gmail.com>
2017-10-04 13:04:35 +0200

    First try of Magento Developing

---
+++ htdocs/app/code/local/Zangirolami/Helloworld/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Hello World
+ */
+
+/**
+ * Hello World
+ *
+ * Main Helper
+ * @author Michael Zangirolami <michael.zangirolami@thinkopen.it>
+ * @package Frontend
+ * @version 0.1.0
+ */
+
+class Zangirolami_Helloworld_Helper_Data extends Mage_Core_Helper_Abstract{
+
+}
---
+++ htdocs/app/code/local/Zangirolami/Helloworld/controllers/IndexController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utente
+ * Date: 04/10/2017
+ * Time: 12:46
+ */
