2238ce55728c70ecb0229a0a18981e93d30bac9e
Michael <mikezangirolami@gmail.com>
2017-10-05 18:04:02 +0200

    New update with new HelloWorld Theme

---
+++ htdocs/app/code/local/Zangirolami/Helloworld/Block/HelloWorld.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * HelloWorld
+ *
+ * Main Helper
+ * @author Michael Zangirolami <michael.zangirolami@thinkopen.it>
+ * @package Frontend
+ * @version 0.1.0
+ */
+
+class Zangirolami_Helloworld_Block_Helloworld extends Mage_Core_Block_Template{
+    
+}
